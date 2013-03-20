de79f9e3f5db519a1645b03de574ad700e295523
Tessa Thornton <tessathornton@gmail.com>
2013-03-20 02:06:56 -0400

    update bio, clean up code

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,81 @@
-$(document).ready(function(){
-  accordion();
-
-  $('#challenges-filter').affix({ offset: $('#challenges-filter').position() });
-
-  $("#all-filter").click(function () {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-    $('.challenges_page_list-challenge').show();
-  });
-  $("#software-filter").click(function () {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-    $('.challenges_page_list-challenge').hide();
-    $('.challenges_page_list-challenge').filter('.software').show();
-  });
-  $("#hardware-filter").click(function () {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-    $('.challenges_page_list-challenge').hide();
-    $('.challenges_page_list-challenge').filter('.hardware').show();
-  });
-  $("#design-filter").click(function () {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-    $('.challenges_page_list-challenge').hide();
-    $('.challenges_page_list-challenge').filter('.design').show();
-  });
-  $("#strategy-filter").click(function () {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-    $('.challenges_page_list-challenge').hide();
-    $('.challenges_page_list-challenge').filter('.strategy').show();
-  });
-});
-
-
 function accordion(){
   var toggle = $(".accordion-trigger");
   var content = $(".accordion-content");
   content.hide();
   toggle.on("click", function(e){
-    if ($(e.target).hasClass("faq-q")){
-      content.slideUp();
-    }
+    content.slideUp();
     e.preventDefault();
     var targetContent = $(this).attr("href");
     $(targetContent).slideToggle();
   });
 }
+
+function scrollNav() {
+  $(".page-nav").on("click", "a", function(e){
+    var target = $(this).attr("href");
+    if (target.charAt(0) === "#"){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 1000);
+    }
+  });
+}
+
+function filter() {
+  if ($("#challenges-filter").length) {
+    $('#challenges-filter').affix({ offset: $('#challenges-filter').position() });
+
+    $("#all-filter").click(function () {
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+      $('.challenges_page_list-challenge').show();
+    });
+    $("#software-filter").click(function () {
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+      $('.challenges_page_list-challenge').hide();
+      $('.challenges_page_list-challenge').filter('.software').show();
+    });
+    $("#hardware-filter").click(function () {
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+      $('.challenges_page_list-challenge').hide();
+      $('.challenges_page_list-challenge').filter('.hardware').show();
+    });
+    $("#design-filter").click(function () {
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+      $('.challenges_page_list-challenge').hide();
+      $('.challenges_page_list-challenge').filter('.design').show();
+    });
+    $("#strategy-filter").click(function () {
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+      $('.challenges_page_list-challenge').hide();
+      $('.challenges_page_list-challenge').filter('.strategy').show();
+    });
+  }
+}
+
+function showMore() {
+  var moreTrigger = $(".more-link");
+  moreTrigger.on("click", function(e){
+    e.preventDefault();
+    if ($(this).text() === "More") {
+      $(this).text("Less");
+    } else {
+      $(this).text("More");
+    }
+    var targetMoreContent = $(this).attr("href");
+    $(targetMoreContent).slideToggle();
+  });
+}
+
+$(document).ready(function(){
+  accordion();
+  filter();
+  showMore();
+  filter();
+  scrollNav();
+});
